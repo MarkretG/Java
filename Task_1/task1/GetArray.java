589f61981c104fdfdf1b6a8cbae4d6e16fb1577b
MarkretG <markretg@gmail.com>
2021-06-30 10:23:47 +0530

    initial commit

--- Task_1/task1/GetArray.java
+++ Task_1/task1/GetArray.java
@@ -1,6 +1,6 @@
 package task1;
 import java.util.Scanner;
-public class Array {
+public class GetArray {
     static Scanner sc=new Scanner(System.in);
     public static int[] getElementsOfNumericArray()
     {
--- Task_1/task1/Task1Runner.java
+++ Task_1/task1/Task1Runner.java
@@ -2,77 +2,77 @@ package task1;
 import java.util.Scanner;
 public class Task1Runner {
     static Scanner sc = new Scanner(System.in);
-    static  int[] InputArray;
+    static  int[] inputArray;
     public static void main(String[] args) {
         System.out.println("1.find index" + "\n" + "2.sum values" + "\n" + "3.remove element"+"\n"+"4.Insert element"+"\n"+"5.Check given element present or not"+"\n"+"6.Find max min"+"\n"+"71.Two string array common elements"+"\n"+"72.two numeric array common elements"+"\n"+"8.remove duplicate"+"\n"+"9.find odd and even count"+"\n"+"10.find Average"+"\n"+"11.find sum of two elements equal to target value"+"\n"+"12.reverse array"+"\n"+"13.Separate even first odd next"+"\n"+"14.numeric sort"+"\n"+"15.find leaders"+"\n"+"16.exit");
         while (true) {
             int ch = sc.nextInt();
             switch (ch) {
                 case 1:
-                    InputArray=Array.getElementsOfNumericArray();
-                    IndexOfElement.findIndex(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    IndexOfElement.findIndex(inputArray);
                     break;
                 case 2:
-                    InputArray=Array.getElementsOfNumericArray();
-                    SumValuesOfNumericArray.sumValues(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    SumValuesOfNumericArray.sumValues(inputArray);
                     break;
                 case 3:
-                    InputArray=Array.getElementsOfNumericArray();
-                    RemoveSpecificElementOnArray.removeElement(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    RemoveSpecificElementOnArray.removeElement(inputArray);
                     break;
                 case 4:
-                    InputArray=Array.getElementsOfNumericArray();
-                    InsertElementAtPositionOnArray.insertElement(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    InsertElementAtPositionOnArray.insertElement(inputArray);
                     break;
                 case 5:
-                    InputArray=Array.getElementsOfNumericArray();
-                    CheckArrayContainSpecificValue.checkArrayContainsOrNot(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    CheckArrayContainSpecificValue.checkArrayContainsOrNot(inputArray);
                     break;
                 case 6:
-                    InputArray=Array.getElementsOfNumericArray();
-                    FindMaxMin.findMaxMin(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    FindMaxMin.findMaxMin(inputArray);
                     break;
                 case 71:
-                    String[] array1=Array.getElementsOfStringArray();
-                    String[] array2=Array.getElementsOfStringArray();
+                    String[] array1=GetArray.getElementsOfStringArray();
+                    String[] array2=GetArray.getElementsOfStringArray();
                     CommonElementBetweenTwoStringArrays.findCommonElement(array1,array2);
                     break;
                 case 72:
-                   int[] array3=Array.getElementsOfNumericArray();
-                   int[] array4=Array.getElementsOfNumericArray();
+                   int[] array3=GetArray.getElementsOfNumericArray();
+                   int[] array4=GetArray.getElementsOfNumericArray();
                     CommonElementBetweenTwoNumericArray.findCommonElement(array3,array4);
                     break;
                 case 8:
-                    InputArray=Array.getElementsOfNumericArray();
-                    RemoveDuplicate.removeDuplicate(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    RemoveDuplicate.removeDuplicate(inputArray);
                     break;
                 case 9:
-                    InputArray=Array.getElementsOfNumericArray();
-                    FindNumberOfOddAndEven.findMaxMinCount(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    FindNumberOfOddAndEven.findMaxMinCount(inputArray);
                     break;
                 case 10:
-                    InputArray=Array.getElementsOfNumericArray();
-                    FindAverageValueExceptMaxMin.findAverage(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    FindAverageValueExceptMaxMin.findAverage(inputArray);
                     break;
                 case 11:
-                    InputArray=Array.getElementsOfNumericArray();
-                    SumOfTwoElementEqualToGivenValue.sumOfTwoElement(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    SumOfTwoElementEqualToGivenValue.sumOfTwoElement(inputArray);
                     break;
                 case 12:
-                    InputArray=Array.getElementsOfNumericArray();
-                    ReverseArray.reverseArray(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    ReverseArray.reverseArray(inputArray);
                     break;
                 case 13:
-                    InputArray=Array.getElementsOfNumericArray();
-                    SeparateOddEven.separateEvenOdd(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    SeparateOddEven.separateEvenOdd(inputArray);
                     break;
                 case 14:
-                    InputArray=Array.getElementsOfNumericArray();
-                    NumericArraySort.sortArray(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    NumericArraySort.sortArray(inputArray);
                     break;
                 case 15:
-                    InputArray=Array.getElementsOfNumericArray();
-                    FindLeadersInArray.findLeaders(InputArray);
+                    inputArray=GetArray.getElementsOfNumericArray();
+                    FindLeadersInArray.findLeaders(inputArray);
                     break;
                 case 16:
                     System.exit(0);
