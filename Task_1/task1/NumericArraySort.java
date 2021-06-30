4f26ac17387f0fd0ea9ba5cc492783e273a10bf6
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-06-30 19:04:35 +0530

    initial commit

--- Task_1/task1/NumericArraySort.java
+++ Task_1/task1/NumericArraySort.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 public class NumericArraySort {
     static Scanner sc=new Scanner(System.in);
     public static void sortArray(int[] array1) {
-        int arraySize=array1.length;
         Arrays.sort(array1);
         for(int i=0;i<arraySize;i++)
             System.out.println(array1[i]);
