e694b4fa7ee15d5d14333ae304a7d6cd294cfabc
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-06-30 18:41:49 +0530

    Update NumericArraySort.java

--- Task_1/task1/NumericArraySort.java
+++ Task_1/task1/NumericArraySort.java
@@ -4,21 +4,7 @@ public class NumericArraySort {
     static Scanner sc=new Scanner(System.in);
     public static void sortArray(int[] array1) {
         int arraySize=array1.length;
-        int temp;
-        // Arrays.sort(array1);
-        for (int i=0;i<arraySize;i++)
-        {
-            for(int j=i+1;j<arraySize;j++)
-            {
-                if(array1[i]>array1[j])
-                {
-                    temp=array1[i];
-                    array1[i]=array1[j];
-                    array1[j]=temp;
-                }
-            }
-        }
-
+        Arrays.sort(array1);
         for(int i=0;i<arraySize;i++)
             System.out.println(array1[i]);
 
