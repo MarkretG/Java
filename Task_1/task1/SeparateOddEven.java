816ac38b22ffe9692e4bdca2514eb1e6f66319ad
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-05 22:58:41 +0530

    initial Commit

--- Task_1/task1/SeparateOddEven.java
+++ Task_1/task1/SeparateOddEven.java
@@ -1,20 +1,23 @@
 package task1;
 public class SeparateOddEven {
     public static void separateEvenOdd(int[] array1) {
-       int arraySize=array1.length;
-        int n=0;
-        for(int i=0;i<arraySize;i++) {
-            if(array1[i]%2==0)
-             {
-               int temp=array1[n];
-               array1[n++]=array1[i];
-               array1[i]=temp;
-            }
-        }
-        for (int j=0;j<arraySize;j++)
+       int[] newArray=new int[arra1.length];
+        int evenCount=0,n=0;
+        for(int i=0;i<array1.length;i++) 
         {
-           System.out.println(array1[j]);
+            if(array1[i]%2==0)
+               evenCount++;      
         }
+        for(int i=0;i<array1.length;i++)
+        {
+            if(arry1[i]%2==0)
+                newArray[n++]=array1[i];
+            else
+                newArray[evenCount++]=array1[i];
+        }
+        for (int j=0;j<evenCount;j++)
+           System.out.println(newArray[j]);
+        
 
     }
 }
