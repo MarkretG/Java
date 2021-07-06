51c4f10c5311c877af5b14512aec1886a8847615
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-06 14:03:54 +0530

    initial commit

--- Task_1/task2/FileCreation.java
+++ Task_1/task2/FileCreation.java
@@ -1,38 +1,44 @@
 package task2;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 public class FileCreation {
     public static void fileCreation(String s)
     {
         File file=new File(s);
-        boolean result;
+        FileWriter fileWriter=null;
         try
         {
-            result=file.createNewFile();
-            if(result)
+            if(!file.isFile())
+            {
+                file.createNewFile();
                 System.out.println("file created successfully");
-            else
-                System.out.println("file name already exit");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-
-            FileWriter fileWriter = new FileWriter(s);
-            fileWriter.write("programmatically created file."+"\n");
-            fileWriter.write("These words are written programmatically."+"\n");
-            fileWriter.write("All is well.Be happy &enjoy the moment"+"\n");
-            fileWriter.close();
+            }
+            fileWriter = new FileWriter(s);
+            fileWriter.write("programmatically created file." + "\n");
+            fileWriter.write("These words are written programmatically." + "\n");
+            fileWriter.write("All is well.Be happy &enjoy the moment" + "\n");
             System.out.println("write successfully");
+
         }
-        catch (IOException i)
+        catch(IOException e)
         {
-            i.printStackTrace();
+            System.out.println("Io exception occurred");
         }
+        finally
+        {
+            try {
+
+                if (fileWriter != null)
+                    fileWriter.close();
+               }
+            catch (IOException  i)
+            {
+                System.out.println("error occurred");
+            }
+
+        }
+
 
     }
 }
