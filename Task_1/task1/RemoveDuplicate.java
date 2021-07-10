267c522617bac48ea01810a4e0f543529c5730e9
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-10 15:13:42 +0530

    initial commit

--- Task_1/task1/RemoveDuplicate.java
+++ Task_1/task1/RemoveDuplicate.java
@@ -2,15 +2,13 @@ package task1;
 import java.util.Arrays;
 public class RemoveDuplicate {
     public  static void  removeDuplicate(int[] array1) {
-        int arraySize=array1.length;
         Arrays.sort(array1);
-        int j=0;
-        for(int i=0;i<arraySize-1;i++)
+        int j=1;
+        for(int i=0;i<array1.length-1;i++)
         {
             if(array1[i]!=array1[i+1])
-                array1[j++]=array1[i];
+                array1[j++]=array1[i+1];
         }
-        array1[j++]=array1[arraySize-1];
         for (int i=0;i<j;i++)
             System.out.println(array1[i]);
     }
